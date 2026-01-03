7f6aac7186965122c26b4513dd9a33165763d75f
Darrylwin <logossoudarryl20@gmail.com>
2026-01-03 21:17:57 +0000

    updated confirm dialog comp

--- src/app/@core/components/confirm-dialog.component.ts
+++ src/app/@core/components/confirm-dialog.component.ts
@@ -1,10 +1,10 @@
-import { Component, Input } from '@angular/core';
-import { NbDialogRef } from '@nebular/theme';
+import { Component, Input } from "@angular/core";
+import { NbDialogRef } from "@nebular/theme";
 
 @Component({
-  selector:  'ngx-confirm-dialog',
+  selector: "ngx-confirm-dialog",
   template: `
-    <nb-card [status]="status">
+    <nb-card>
       <nb-card-header>{{ title }}</nb-card-header>
       <nb-card-body>
         <div [innerHTML]="message"></div>
@@ -13,33 +13,36 @@ import { NbDialogRef } from '@nebular/theme';
         <button nbButton status="basic" (click)="cancel()">
           {{ cancelText }}
         </button>
-        <button nbButton [status]="status" (click)="confirm()">
+        <button nbButton [attr.status]="status" (click)="confirm()">
           {{ confirmText }}
         </button>
       </nb-card-footer>
     </nb-card>
   `,
-  styles: [`
-    nb-card {
-      margin:  0;
-      max-width: 500px;
-    }
-    
-    nb-card-footer {
-      display: flex;
-      justify-content: flex-end;
-      gap: 1rem;
-    }
-  `]
+  styles: [
+    `
+      nb-card {
+        margin: 0;
+        max-width: 500px;
+      }
+
+      nb-card-footer {
+        display: flex;
+        justify-content: flex-end;
+        gap: 1rem;
+      }
+    `,
+  ],
 })
 export class ConfirmDialogComponent {
-  @Input() title: string = 'Confirmation';
-  @Input() message: string = 'Êtes-vous sûr ? ';
-  @Input() confirmText: string = 'Confirmer';
-  @Input() cancelText: string = 'Annuler';
-  @Input() status: 'primary' | 'success' | 'info' | 'warning' | 'danger' = 'danger';
+  @Input() title: string = "Confirmation";
+  @Input() message: string = "Êtes-vous sûr ?";
+  @Input() confirmText: string = "Confirmer";
+  @Input() cancelText: string = "Annuler";
+  @Input() status: "primary" | "success" | "info" | "warning" | "danger" =
+    "danger";
 
-  constructor(protected ref:  NbDialogRef<ConfirmDialogComponent>) {}
+  constructor(protected ref: NbDialogRef<ConfirmDialogComponent>) {}
 
   cancel() {
     this.ref.close(false);
