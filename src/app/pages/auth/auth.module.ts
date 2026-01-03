8ecdba8aa7a4e5205215288b9f8d65875a583363
Darrylwin <logossoudarryl20@gmail.com>
2026-01-03 21:19:43 +0000

    corrected login comps

--- src/app/pages/auth/auth.module.ts
+++ src/app/pages/auth/auth.module.ts
@@ -1,40 +1,30 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 // Modules Nebular
-import { 
-  NbAuthModule, 
-  NbLoginComponent, 
-  NbRegisterComponent, 
-  NbLogoutComponent, 
-  NbRequestPasswordComponent, 
-  NbResetPasswordComponent 
-} from '@nebular/auth';
-import { 
-  NbAlertModule, 
-  NbButtonModule, 
-  NbCardModule, 
-  NbCheckboxModule, 
-  NbIconModule, 
-  NbInputModule, 
-  NbLayoutModule, 
-  NbSpinnerModule 
-} from '@nebular/theme';
+import {
+  NbAlertModule,
+  NbButtonModule,
+  NbCardModule,
+  NbCheckboxModule,
+  NbIconModule,
+  NbInputModule,
+  NbLayoutModule,
+  NbSpinnerModule,
+} from "@nebular/theme";
 
-import { AuthRoutingModule } from './auth-routing.module';
-import { LoginComponent } from './login/login.component';
+import { AuthRoutingModule } from "./auth-routing.module";
+import { LoginComponent } from "./login/login.component";
 
 @NgModule({
-  declarations: [
-    LoginComponent
-  ],
+  declarations: [LoginComponent],
   imports: [
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
     AuthRoutingModule,
-    
+
     // Modules Nebular UI
     NbAlertModule,
     NbButtonModule,
@@ -44,12 +34,6 @@ import { LoginComponent } from './login/login.component';
     NbInputModule,
     NbLayoutModule,
     NbSpinnerModule,
-    
-    // Module Auth Nebular (optionnel - on va créer notre propre login)
-    // NbAuthModule.forRoot({
-    //   strategies: [],
-    //   forms: {},
-    // }),
-  ]
+  ],
 })
-export class AuthModule { }
+export class AuthModule {}
