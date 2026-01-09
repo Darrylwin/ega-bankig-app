33df535bf6437146ae5910e2567dee481d137b30
Darrylwin <logossoudarryl20@gmail.com>
2026-01-09 08:33:39 +0000

    updated api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080/api', // URL de production
+  apiUrl: 'http://89.187.7.35:25577/api', // backend Spring Boot
   appName: 'EGA Bank Administration',
   version: '1.0.0'
 };
