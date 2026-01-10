32d352fbbb7fb29c98da89c064e33e817feebce3
Darrylwin <logossoudarryl20@gmail.com>
2026-01-10 16:36:42 +0000

    Add HTTPS proxy for API

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: '/api',  // ← CHANGER ICI ! Seulement '/api'
+  apiUrl: '/api',
   appName: 'EGA Bank Administration',
   version: '1.0.0'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://89.187.7.35:25577/api', // backend Spring Boot
+  production: true,
+  apiUrl: '/api',
   appName: 'EGA Bank Administration',
   version: '1.0.0'
 };
