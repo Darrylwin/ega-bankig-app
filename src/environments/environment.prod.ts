d8c59351fcaa392109a2dea9a04929c3aaae4804
Darrylwin <logossoudarryl20@gmail.com>
2026-01-10 15:29:48 +0000

    fix: use relative API URL for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://89.187.7.35:25577/api', // backend Spring Boot
+  apiUrl: '/api',  // ← CHANGER ICI ! Seulement '/api'
   appName: 'EGA Bank Administration',
   version: '1.0.0'
 };
